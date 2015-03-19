dc19dabd65d815da74697ff2e54cc4b12cf1ab38
guru0323 <bho88207.horseb086@gmail.com>
2015-03-20 01:56:43 +0200

    use truffle for tests and add solidity-coverage

--- .solcover.js
+++ .solcover.js
@@ -1,8 +1,11 @@
 module.exports = {
   norpc: true,
+
   compileCommand: '../node_modules/.bin/truffle compile',
+
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   copyPackages: ['openzeppelin-solidity'],
+
   providerOptions: {
     accounts: [
       { secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200', balance: '0xD3C21BCECCEDA1000000' },
@@ -18,4 +21,5 @@ module.exports = {
       { secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501210', balance: '0xD3C21BCECCEDA1000000' },
     ],
   }
+  
 };
